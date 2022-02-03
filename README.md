3af2553c69757a5a8c555fec4ec9c65af308edd9
Mike Brown <github@torvosoft.com>
2022-02-03 06:06:50 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# examples
-Example Bopmatic Projects
+# Bopmatic Examples
+
+The Bopmatic Examples repository is a collection of public domain
+Bopmatic projects showing how to use the service. Anyone is free to
+copy/paste/modify any of the examples to use in their own projects.
+
+## Building
+
+bopmatic build <project>
+
+## Installing
+
+Any of the examples here can be installed manually by cloning from the
+repository or installed via the
+[Bopmatic CLI](https://github.com/bopmatic/cli). e.g.:
+
+```bash
+bopmatic new --template helloworld
+```
+
+## Usage
+
+N/A
+
+## Contributing
+Pull requests are welcome at https://github.com/bopmatic/examples
+
+For major changes, please open an issue first to discuss what you
+would like to change.
+
+## License
+[UNLICENSE](https://unlicense.org)
---
+++ golang/helloworld/cmd/greeter_server/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/bopmatic/examples/golang/helloworld/pb"
+	"google.golang.org/grpc"
+)
+
+var (
+	// default port here should match what you've defined in Bopmatic.yaml
+	port = flag.Int("port", 26001, "The server port")
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("SayHello: Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello from Bopmatic " + in.GetName() + "!"}, nil
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
